0c71058ad3b2a62a72419b9c7d27bba048220628
Joshua Ellis <71731580+JoshuaEll@users.noreply.github.com>
2022-08-28 22:32:48 -0500

    added project files

---
+++ index.js
@@ -0,0 +1,16 @@
+
+let count = 0
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+
+function increment(){
+    count  +=  1
+    countEl.innerText = count
+}
+
+function save(){
+    let saveCount = count + " - "
+    saveEl.textContent += saveCount
+    countEl.innerText = 0
+    count = 0
+}
